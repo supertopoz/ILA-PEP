418c912a7085cd66972eb97b4629fe7f4d6124cb
supertopoz <deepsleepsounds@gmail.com>
2018-09-19 07:31:21 +0900

    add animated menu to redux

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import {connect} from "react-redux";
 import styled from "styled-components"
+
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 import  Search from '../Search/Search';
-
+import { showSideMenu } from "../../actions/pageAnimations";
 const FixedHeader = styled.div`
   display: grid;
   grid-gap:10px;
@@ -36,10 +37,11 @@ class Header extends React.Component {
       super(props)
     }
     render(){
+      console.log('header props', this.props)
     return (
         <FixedHeader>
           { console.log('These are props for the header:', this.props)}
-          <Menu><i className="material-icons">menu</i></Menu>                
+          <Menu onClick={()=> this.props.showSideMenu(true)}><i className="material-icons">menu</i></Menu>                
           <Link to="/home"><Logo src={logoImg}/></Link>
           <Search />
           <div></div>
@@ -56,14 +58,16 @@ class Header extends React.Component {
 
     const mapStateToProps = (state) => {
       return {
-        student: state.student
+        student: state.student,
+        sideMenu: state.pageAnimations.sideMenu
+
       };
     };
 
     const mapDispatchToProps = (dispatch) => {
       return {
-        addStudentId: (id) => { dispatch(addStudentId(id)) },
-        
+        addStudentId: (id) => { dispatch(addStudentId(id))},
+        showSideMenu: (visibility) => { dispatch(showSideMenu(visibility))}   
     };
   }
 
